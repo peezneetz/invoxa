ceefd417a7ee8f25fa78f5eec7d4f5bd32f4041f
HZ <_>
2026-05-27 13:51:34 +0700

    backend URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: 'https://invoxa-backend-xxxxx-uc.a.run.app/api',
 });
 
 api.interceptors.request.use((config) => {
